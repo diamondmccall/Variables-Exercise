f884207b335a0182357c7d4671b3eb06af6cc391
Diamond Mccall <singlikediamond@gmail.com>
2023-01-13 11:35:28 -0600

    inital commits

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -4,7 +4,30 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; // declaration syntax
+
+            dogName = "Jordan";// initalize syntax --- assigning a value
+
+            int dogAge = 5; // declairing and initzaliting
+
+            char firstInital = 'J'; // declairing and initializing
+
+            bool isHungry = true; // declairing and initalizing
+
+            double dogWeight = 9.7; // declairing and initalinzing
+
+            decimal dogHeight = 7.8m; //declairing and initaling
+
+
+            Console.WriteLine($"My dog's name is {dogName},He is {dogAge} years old. ");
+               
+            Console.WriteLine ($"He is {dogWeight} pounds and {dogHeight} inches tall.");
+
+            Console.WriteLine($"It is {isHungry} that {firstInital} always seems hungry.");
+
+
+
+    
         }
     }
 }
